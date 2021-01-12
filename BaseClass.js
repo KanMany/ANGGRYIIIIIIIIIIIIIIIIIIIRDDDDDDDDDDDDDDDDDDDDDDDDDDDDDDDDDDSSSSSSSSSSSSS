e4d06e499ea3f21aa386e2588d5c1fa7e2032828
KANISHK MANJUNATH <11kanishk@gmail.com>
2021-01-12 19:59:43 +0000

    piggy is thinner

--- BaseClass.js
+++ BaseClass.js
@@ -6,7 +6,7 @@ class BaseClass{
             'density':1.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
+        this.width = width/2;
         this.height = height;
         this.image = loadImage("sprites/base.png");
         World.add(world, this.body);
